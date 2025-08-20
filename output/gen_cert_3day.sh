55d9ca906de92137bae58419a02067b5ff907fbf
john-4715 <john.he@copeland.com>
2025-08-20 13:20:17 +0800

    delete relative certificate

--- output/gen_cert_3day.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Hello"
-
-# Client
-openssl genrsa -out client_3day.key 2048
-openssl req -new -out client_3day.csr -key client_3day.key -subj "/C=CN/ST=Shanxi/L=Xian/O=EXEC/OU=DS/CN=client_3day_ip"
-openssl x509 -req -in client_3day.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client_3day.crt -days 3
-openssl rsa -in client_3day.key -out client_3day.key
